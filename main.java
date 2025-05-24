10df357924aa1bf61ba6afb3735e9b33d5888681
Yash Pal <bagheldeepanshu7451@gmail.com>
2025-05-24 06:29:05 +0530

    2 problem solved

---
+++ main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args){
+        Scanner sic =new Scanner(System.in);
+        pattern1(sic.nextInt());
+        pattern2(sic.nextInt());
+    }
+
+
+    static void pattern2 (int n){
+        for (int row = 1 ; row <= n ; row++){
+            for (int col =1 ; col<=n; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+
+
+
+    static void pattern1 (int n){
+        for (int row =1 ; row <= n ; row++){
+            for (int col =1 ; col <= row ; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
